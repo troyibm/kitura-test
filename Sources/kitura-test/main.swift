8e16c576c154d2ee6d427589e7926f72907de9eb
TD <tdugger@TDs-MacBook-Pro.local>
2018-06-23 17:23:19 -0500

    initial commit

---
+++ Sources/kitura-test/main.swift
@@ -0,0 +1,21 @@
+import Kitura
+
+// Create a new router
+let router = Router()
+
+// Handle HTTP GET requests to /
+router.get("/") { request, response, next in
+    do {
+        try response.send(fileName: "demo.html")
+    } catch let error {
+        print(error.localizedDescription)
+    }
+    next()
+}
+
+// Add an HTTP server and connect it to the router
+Kitura.addHTTPServer(onPort: 8080, with: router)
+
+// Start the Kitura runloop (this call never returns)
+Kitura.run()
+
